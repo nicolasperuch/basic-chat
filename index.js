8be1912f6d6b4fe50e2286da5029fd437ad60992
Nicolas Peruch <nicolasperuch.2010@gmail.com>
2018-08-12 13:23:30 -0300

    creating basic chat using socket io

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const socket = require('socket.io');
+
+const app = express();
+
+const server = app.listen(4000, () => {
+    console.log('Listening to 4000')
+})
+
+app.use(express.static('public'))
+
+
+const io = socket(server);
+
+io.on('connection', (socket) => {
+    console.log('made socket connection', socket.id)
+
+    socket.on('chat', (data) => {
+        io.sockets.emit('chat', data);
+    })
+
+    socket.on('typing', (data) => {
+        socket.broadcast.emit('typing', data);
+    })
+});
